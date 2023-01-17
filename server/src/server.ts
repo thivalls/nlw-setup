0039d72f711369a87ca0a4c5740aed1ba5428824
valls <olavalls@gmail.com>
2023-01-17 14:46:40 -0300

    Creating backend structure

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,17 @@
-console.log('running...')
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get("/dados", () => {
+    const all = prisma.habit.findMany()
+    return all
+})
+
+app.listen({
+    port: 3333
+})
